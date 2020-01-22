0acc094e13d1e0527188623ff0d45baf55e482ee
siddharthpandit <siddharth10p@gmail.com>
2020-01-23 00:40:52 +0530

    adding field enable / diable js

--- payments/authnet-web-elements-card/pay.js
+++ payments/authnet-web-elements-card/pay.js
@@ -178,6 +178,125 @@ function sendPaymentDataToAnet(elements, example) {
 	var error = form.querySelector('.error');
 	var errorMessage = error.querySelector('.message');
 
+	function enableInputs() {
+		Array.prototype.forEach.call(
+			form.querySelectorAll(
+				"input[type='text'], input[type='email'], input[type='tel']"
+			),
+			function(input) {
+				input.removeAttribute('disabled');
+			}
+		);
+	}
+
+	function disableInputs() {
+		Array.prototype.forEach.call(
+			form.querySelectorAll(
+				"input[type='text'], input[type='email'], input[type='tel']"
+			),
+			function(input) {
+				input.setAttribute('disabled', 'true');
+			}
+		);
+	}
+
+	function triggerBrowserValidation() {
+		// The only way to trigger HTML5 form validation UI is to fake a user submit event
+		var submit = document.createElement('input');
+		submit.type = 'submit';
+		submit.style.display = 'none';
+		form.appendChild(submit);
+		submit.click();
+		submit.remove();
+	}
+
+	// Listen for errors from each Element, and show error messages in the UI.
+	var savedErrors = {};
+	elements.forEach(function(element, idx) {
+		element.on('change', function(event) {
+			if (event.error) {
+				error.classList.add('visible');
+				savedErrors[idx] = event.error.message;
+				errorMessage.innerText = event.error.message;
+			} else {
+				savedErrors[idx] = null;
+
+				// Loop over the saved errors and find the first one, if any.
+				var nextError = Object.keys(savedErrors)
+				.sort()
+				.reduce(function(maybeFoundError, key) {
+					return maybeFoundError || savedErrors[key];
+				}, null);
+
+				if (nextError) {
+					// Now that they've fixed the current error, show another one.
+					errorMessage.innerText = nextError;
+				} else {
+					// The user fixed the last error; no more errors.
+					error.classList.remove('visible');
+				}
+			}
+		});
+	});
+
+	resetButton.addEventListener('click', function(e) {
+		e.preventDefault();
+		// Resetting the form (instead of setting the value to `''` for each input)
+		// helps us clear webkit autofill styles.
+		form.reset();
+
+		// Clear each Element.
+		elements.forEach(function(element) {
+			element.clear();
+		});
+
+		// Reset error state as well.
+		error.classList.remove('visible');
+
+		// Resetting the form does not un-disable inputs, so we need to do it separately:
+		enableInputs();
+		example.classList.remove('submitted');
+	});
+
+	// Listen on the form's 'submit' handler - here's where all the magic happens
+	form.addEventListener('submit', function(e) {
+		e.preventDefault();
+
+		// Trigger HTML5 validation UI on the form if any of the inputs fail validation
+		var plainInputsValid = true;
+		Array.prototype.forEach.call(form.querySelectorAll('input'), function(input) {
+			if (input.checkValidity && !input.checkValidity()) {
+				plainInputsValid = false;
+				return;
+			}
+		});
+		if (!plainInputsValid) {
+			triggerBrowserValidation();
+			return;
+		}
+
+		// Show a loading screen...
+		example.classList.add('submitting');
+
+		// Disable all inputs.
+		disableInputs();
+
+		// Gather additional customer data we may have collected in our form.
+	/*	var name = form.querySelector('#' + exampleName + '-name');
+		var email = form.querySelector('#' + exampleName + '-email');
+		var address1 = form.querySelector('#' + exampleName + '-address');
+		var city = form.querySelector('#' + exampleName + '-city');
+		var state = form.querySelector('#' + exampleName + '-state');
+		var zip = form.querySelector('#' + exampleName + '-zip');
+		var additionalData = {
+			name: name ? name.value : undefined,
+			email: email ? email.value : undefined,
+			address_line1: address1 ? address1.value : undefined,
+			address_city: city ? city.value : undefined,
+			address_state: state ? state.value : undefined,
+			address_zip: zip ? zip.value : undefined,
+		};*/
+	});
 	// Show a loading screen...
 	example.classList.add('submitting');
 
@@ -193,22 +312,10 @@ function sendPaymentDataToAnet(elements, example) {
         cardData.cardCode = elements[2];
 
 		console.log('cardData ===> ' + cardData);
-    // If using banking information instead of card information,
-    // build a bankData object instead of a cardData object.
-    //
-    // var bankData = {};
-    //     bankData.accountNumber = document.getElementById('accountNumber').value;
-    //     bankData.routingNumber = document.getElementById('routingNumber').value;
-    //     bankData.nameOnAccount = document.getElementById('nameOnAccount').value;
-    //     bankData.accountType = document.getElementById('accountType').value;
 
     var secureData = {};
         secureData.authData = authData;
         secureData.cardData = cardData;
-        // If using banking information instead of card information,
-        // send the bankData object instead of the cardData object.
-        //
-        // secureData.bankData = bankData;
 
     Accept.dispatchData(secureData, responseHandler);
 
@@ -223,7 +330,25 @@ function sendPaymentDataToAnet(elements, example) {
                 i = i + 1;
             }
         } else {
-            sendTokenBlackthornPaymentsAPI(response, example);
+			var name = form.querySelector('#' + exampleName + '-name');
+			var email = form.querySelector('#' + exampleName + '-email');
+		//	var address1 = form.querySelector('#' + exampleName + '-address');
+		//	var city = form.querySelector('#' + exampleName + '-city');
+		//	var state = form.querySelector('#' + exampleName + '-state');
+		//	var zip = form.querySelector('#' + exampleName + '-zip');
+		var additionalData = {
+			name: name ? name.value : undefined,
+			email: email ? email.value : undefined,
+			opaqueData: response.opaqueData
+		//	address_line1: address1 ? address1.value : undefined,
+		//	address_city: city ? city.value : undefined,
+		//	address_state: state ? state.value : undefined,
+		//	address_zip: zip ? zip.value : undefined,
+		};
+			var pmData = {
+				paymentMethodData: additionalData
+			}
+            sendTokenBlackthornPaymentsAPI(pmData, example);
         }
     }
 }
