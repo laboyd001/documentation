5736b69809c0207022f2616bbc2a9219855ba859
siddharthpandit <siddharth10p@gmail.com>
2020-02-13 22:59:44 +0530

    fixed conflicts

--- payments/authnet-accept-js-card/pay.js
+++ payments/authnet-accept-js-card/pay.js
@@ -79,7 +79,7 @@ function sendPaymentDataToAnet(exampleName) {
 		}
 
 		// Show a loading screen...
-		example.classList.add('submitting');
+	//	example.classList.add('submitting');
 
 		// Disable all inputs.
 		disableInputs();
@@ -89,7 +89,7 @@ function sendPaymentDataToAnet(exampleName) {
 	function sendDataToAuthNet() {
 		var formClass = '.' + 'example2';
 		var example = document.querySelector(formClass);
-
+	
 		var form = example.querySelector('form');
 		var resetButton = example.querySelector('a.reset');
 		var error = form.querySelector('.error');
@@ -97,7 +97,7 @@ function sendPaymentDataToAnet(exampleName) {
 		var authData = {};
 			authData.clientKey = publicClientKey;
 			authData.apiLoginID = apiLoginID;
-
+	
 		var cardData = {};
 			var cc = form.querySelector('#' + 'example2' + '-card-number').value;
 			cardData.cardNumber = cc.replace(/ /g,'');
@@ -107,198 +107,12 @@ function sendPaymentDataToAnet(exampleName) {
 			cardData.year = cardArray[1];
 			cardData.cardCode = form.querySelector('#' + 'example2' + '-card-cvc').value;
 			console.log('cardData ===> ' + JSON.stringify(cardData));
-
+	
 		var secureData = {};
 			secureData.authData = authData;
 			secureData.cardData = cardData;
-
-<<<<<<< HEAD
-function sendPaymentDataToAnet(exampleName) {
-	var formClass = '.' + exampleName;
-	var example = document.querySelector(formClass);
-
-	var form = example.querySelector('form');
-	var resetButton = example.querySelector('a.reset');
-	var error = form.querySelector('.error');
-	var errorMessage = error.querySelector('.message');
-
-	function enableInputs() {
-		Array.prototype.forEach.call(
-			form.querySelectorAll(
-				"input[type='text'], input[type='email'], input[type='tel']"
-			),
-			function(input) {
-				input.removeAttribute('disabled');
-			}
-		);
-	}
-
-	function disableInputs() {
-		Array.prototype.forEach.call(
-			form.querySelectorAll(
-				"input[type='text'], input[type='email'], input[type='tel']"
-			),
-			function(input) {
-				input.setAttribute('disabled', 'true');
-			}
-		);
-	}
-
-	function triggerBrowserValidation() {
-		// The only way to trigger HTML5 form validation UI is to fake a user submit event
-		var submit = document.createElement('input');
-		submit.type = 'submit';
-		submit.style.display = 'none';
-		form.appendChild(submit);
-		submit.click();
-		submit.remove();
-	}
-
-	// Listen for errors from each Element, and show error messages in the UI.
-/*	var savedErrors = {};
-	elements.forEach(function(element, idx) {
-		element.on('change', function(event) {
-			if (event.error) {
-				error.classList.add('visible');
-				savedErrors[idx] = event.error.message;
-				errorMessage.innerText = event.error.message;
-			} else {
-				savedErrors[idx] = null;
-
-				// Loop over the saved errors and find the first one, if any.
-				var nextError = Object.keys(savedErrors)
-				.sort()
-				.reduce(function(maybeFoundError, key) {
-					return maybeFoundError || savedErrors[key];
-				}, null);
-
-				if (nextError) {
-					// Now that they've fixed the current error, show another one.
-					errorMessage.innerText = nextError;
-				} else {
-					// The user fixed the last error; no more errors.
-					error.classList.remove('visible');
-				}
-			}
-		});
-	});
-
-	resetButton.addEventListener('click', function(e) {
-		e.preventDefault();
-		// Resetting the form (instead of setting the value to `''` for each input)
-		// helps us clear webkit autofill styles.
-		form.reset();
-
-		// Clear each Element.
-		elements.forEach(function(element) {
-			element.clear();
-		});
-
-		// Reset error state as well.
-		error.classList.remove('visible');
-
-		// Resetting the form does not un-disable inputs, so we need to do it separately:
-		enableInputs();
-		example.classList.remove('submitted');
-	});*/
-
-	// Listen on the form's 'submit' handler - here's where all the magic happens
-	form.addEventListener('submit', function(e) {
-		e.preventDefault();
-
-		// Trigger HTML5 validation UI on the form if any of the inputs fail validation
-		var plainInputsValid = true;
-		Array.prototype.forEach.call(form.querySelectorAll('input'), function(input) {
-			if (input.checkValidity && !input.checkValidity()) {
-				plainInputsValid = false;
-				return;
-			}
-		});
-		if (!plainInputsValid) {
-			triggerBrowserValidation();
-			return;
-		}
-
 		// Show a loading screen...
 		example.classList.add('submitting');
-
-		// Disable all inputs
-		//disableInputs();
-
-		// Gather additional customer data we may have collected in our form.
-		var name = form.querySelector('#' + exampleName + '-name');
-		var email = form.querySelector('#' + exampleName + '-email');
-		var address1 = form.querySelector('#' + exampleName + '-address');
-		var city = form.querySelector('#' + exampleName + '-city');
-		var state = form.querySelector('#' + exampleName + '-state');
-		var zip = form.querySelector('#' + exampleName + '-zip');
-		var additionalData = {
-			name: name ? name.value : undefined,
-			email: email ? email.value : undefined,
-			address_line1: address1 ? address1.value : undefined,
-			address_city: city ? city.value : undefined,
-			address_state: state ? state.value : undefined,
-			address_zip: zip ? zip.value : undefined,
-		};
-
-	});
-    var authData = {};
-        authData.clientKey = publishableKey;
-        authData.apiLoginID = apiLoginID;
-
-    var cardData = {};
-		cardData.cardNumber = form.querySelector('#' + exampleName + '-card-number');
-		var cardExpiry = form.querySelector('#' + exampleName + '-card-expiry');
-		console.log('cardExpiry ==> ' + cardExpiry);
-        cardData.month = cardExpiry;
-        cardData.year = cardExpiry;
-        cardData.cardCode = form.querySelector('#' + exampleName + '-card-cvc');
-
-		console.log('cardData ===> ' + cardData);
-
-    var secureData = {};
-        secureData.authData = authData;
-        secureData.cardData = cardData;
-
-    //Accept.dispatchData(secureData, responseHandler);
-
-    function responseHandler(response) {
-        if (response.messages.resultCode === "Error") {
-            var i = 0;
-            while (i < response.messages.message.length) {
-                console.log(
-                    response.messages.message[i].code + ": " +
-                    response.messages.message[i].text
-                );
-                i = i + 1;
-            }
-        } else {
-			var name = form.querySelector('#' + exampleName + '-name');
-			var email = form.querySelector('#' + exampleName + '-email');
-		//	var address1 = form.querySelector('#' + exampleName + '-address');
-		//	var city = form.querySelector('#' + exampleName + '-city');
-		//	var state = form.querySelector('#' + exampleName + '-state');
-		//	var zip = form.querySelector('#' + exampleName + '-zip');
-		var additionalData = {
-			name: name ? name.value : undefined,
-			email: email ? email.value : undefined,
-			opaqueData: response.opaqueData
-		//	address_line1: address1 ? address1.value : undefined,
-		//	address_city: city ? city.value : undefined,
-		//	address_state: state ? state.value : undefined,
-		//	address_zip: zip ? zip.value : undefined,
-		};
-			var pmData = {
-				paymentMethodData: additionalData
-			}
-            sendTokenBlackthornPaymentsAPI(pmData, example);
-        }
-    }
-}
-=======
-		// Show a loading screen...
-		example.classList.add('submitting');
->>>>>>> a421bd0c7f63987f3e8b7734bcdb5c174743774d
 
 		Accept.dispatchData(secureData, 'responseHandler');
 	}
@@ -355,7 +169,7 @@ function sendPaymentDataToAnet(exampleName) {
 			xhr.onload = function() {
 				if (xhr.status === 200) {
 					console.log('paymentsRestAPIResponse: ' + xhr.responseText);
-
+	
 					var response = JSON.parse(xhr.responseText);
 					console.log('response.success: ' + response.success);
 					var msg;
@@ -369,10 +183,10 @@ function sendPaymentDataToAnet(exampleName) {
 					alert(msg);
 					console.log('msg ==> ' + msg);
 					document.getElementById('salesforce_message').innerText = msg;
-
+	
 					// Stop loading!
 					example.classList.remove('submitting');
-
+	
 					// todo: figure out how to show error below form instead of on success page
 					example.classList.add('submitted');
 				}
@@ -409,33 +223,32 @@ function sendPaymentDataToAnet(exampleName) {
 	sendPaymentDataToAnet('example2');
 })();
 
-
 function cc_format(value) {
 	var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
 	var matches = v.match(/\d{4,16}/g);
 	var match = matches && matches[0] || ''
 	var parts = []
 	for (var i=0, len=match.length; i<len; i+=4) {
-		parts.push(match.substring(i, i+4))
+	  parts.push(match.substring(i, i+4))
 	}
 	if (parts.length) {
-		return parts.join(' ')
+	  return parts.join(' ')
 	} else {
-		return value
+	  return value
 	}
-}
+  }
 
-function cc_expiryMonthAndYear(value) {
+  function cc_expiryMonthAndYear(value) {
 	var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
 	var matches = v.match(/\d{2,4}/g);
 	var match = matches && matches[0] || ''
 	var parts = []
 	for (var i=0, len=match.length; i<len; i+=2) {
-		parts.push(match.substring(i, i+2))
+	  parts.push(match.substring(i, i+2))
 	}
 	if (parts.length) {
-		return parts.join('/')
+	  return parts.join('/')
 	} else {
-		return value
+	  return value
 	}
-}
+  }
